c637ab2242cc13e5061c81f403a9e6d71ba22d7c
be55am <sahlibessam@gmail.com>
2018-10-17 10:15:17 +0100

    the first commit a simple tow guru objects in a spring project

---
+++ src/main/java/com/wissal/springtuto/SpringtutoApplication.java
@@ -0,0 +1,12 @@
+package com.wissal.springtuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtutoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wissal/springtuto/SpringtutoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wissal.springtuto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringtutoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
