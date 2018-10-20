162d8c0f3edc99e125189b3a986ab5384ea96a16
be55am <sahlibessam@gmail.com>
2018-10-20 15:27:48 +0100

    the use of external properties

--- src/main/java/com/wissal/DiDemoApplication.java
+++ src/main/java/com/wissal/DiDemoApplication.java
@@ -1,6 +1,7 @@
 package com.wissal;
 
 import com.wissal.controllers.MyController;
+import com.wissal.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,7 +12,9 @@ public class DiDemoApplication {
     public static void main(String[] args) {
         ApplicationContext context=SpringApplication.run(DiDemoApplication.class,args);
 
-        MyController controller=(MyController)context.getBean("myController");
-        System.out.println(controller.hello());
+        FakeDataSource fakeDataSource=(FakeDataSource)context.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getName());
+
     }
 }
